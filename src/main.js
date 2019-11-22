1452c23e6896c21d4e532c8f2cf763ed1e06931f
Ugonz86 <ugonzalez86@gmail.com>
2019-11-22 15:56:49 -0800

    Add user interface logic for the first backend class

--- src/main.js
+++ src/main.js
@@ -1,16 +1,34 @@
 import './styles.css';
 import 'bootstrap';
-// import $ from 'jquery';
-// import {practiceLookup} from './backend.js';
-// import {illnessLookup} from './backend.js';
+import $ from 'jquery';
+import {PracticeLookup} from './backend.js';
 
+$(document).ready(function() {
+  $('#practiceButton').click(function() {
 
+    const practice = $('#practices').val();
+    $('#practices').val("");
 
+    (async () => {
+      // The API call is business logic.
+      let doctorLookupVariable = new PracticeLookup();
+      const response = await doctorLookupVariable.getDoctorByPractice(practice);
+      getElements(response);
+    })();
 
-// $(document).ready(function() {
-//
-// });
-//
-// $("").click(function() {
-//
-// });
+    function getElements(response) {
+      $('.showList').prepend(`Here's the <em><strong>${practice}</strong></em> list we found in Seattle:`);
+      response.practice.forEach ((practice) => {
+        const id = practice.id;
+        const clinic_name = practice.clinic_name;
+        // const firstName = practiceInfo.data.doctors.profile.first_name;
+        // const lastName = practiceInfo.data[i].doctors[0].profile.last_name;
+        //
+        const visit_address = practice.visit_address.street + practice.data.visit_address.street2 + " " + practice.data.visit_address.zip;
+        $('#listDisplay').append(`<li>id: ${id} , clinic name: ${clinic_name}, address: ${visit_address}</li>`);
+        $("#resultDiv").show();
+        $("#practiceDiv").hide();
+      });
+    }
+  });
+});
