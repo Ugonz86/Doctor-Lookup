9cb744aa3cb6830ec02e6dcd3e4f0cc174324eaa
Ugonz86 <ugonzalez86@gmail.com>
2019-11-22 12:27:04 -0800

    Add basic layout and build.

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import './styles.css';
+import 'bootstrap';
+// import $ from 'jquery';
+// import {practiceLookup} from './backend.js';
+// import {illnessLookup} from './backend.js';
+
+
+
+
+// $(document).ready(function() {
+//
+// });
+//
+// $("").click(function() {
+//
+// });
